7e69b076dc05321e6bd58eb0111f194a4f5114b1
Erwan <erwan.pannier@centralesupelec.fr>
2020-12-27 22:54:59 +0100

    working version with tests using HITRAN database

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,8 +1,9 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
-from radis import calc_spectrum
+from radis import calc_spectrum, SpectrumFactory
 from radis import get_version
+from radis.misc.printer import printm
 
 class CO2:
     """
@@ -12,7 +13,7 @@ class CO2:
     """
     
     def setup(self):
-        self.test_options = {
+        self.test_options = opt = {
             "wavelength_min":4165,
             "wavelength_max":4200,
             "databank":"fetch",  # not appropriate for these temperatures, but convenient for automatic testing
@@ -35,27 +36,81 @@ class CO2:
             }}
         
         # Backward compatibility
+        # ----------------------
+        
+        # Old version of RADIS do not necessary work with the latest parameters
+        # Fix it : 
         version = get_version(add_git_number=False)
         if version < '0.9.26':
             del self.test_options['optimization']
-    
-        # Initial calculation to download databases etc. 
-        first_run_options = self.test_options.copy()
-        first_run_options['use_cached'] = 'regen'
-        calc_spectrum(**first_run_options)
         
-    def time_CO2noneq(self):
-
+        # Also fix problems with cache files :
+        
+        # First run to check there are no problems with Line database cache-files
+        # ... Note @dev : as of 0.9.26 encountering a cache file generated with a future version
+        # ... raises an error with no option to automatically regenerate the cache file
+        sf = SpectrumFactory(**{k:v for (k,v) in opt.items() if k in [
+            'wavelength_min','wavelength_max', 'molecule', 'isotope', 'broadening_max_width',
+            'medium']})
+        
+        try:
+            sf.fetch_databank()
+        except ValueError as err:
+            if "generated with a future version" in str(err) and 'tempfile' in str(err):
+                # Clean problematic cache files
+                # Note @dev : in 0.9.25 fetch_astroquery() doesnt use the use_cache
+                # parameter from SpectrumFactory... Too bad, else we could have simply
+                # used the 'regen' value
+                from astroquery.hitran import Hitran
+                import os
+                from os.path import exists, join
+                from radis.io.query import CACHE_FILE_NAME
+                for iso in sf._get_isotope_list(molecule=opt['molecule']):
+                    fcache = join(
+                        Hitran.cache_location,
+                        CACHE_FILE_NAME.format(**{
+                        'molecule':opt['molecule'],'isotope':iso,
+                        'wmin':sf.params.wavenum_min_calc,
+                        'wmax':sf.params.wavenum_max_calc}),
+                    )
+                    printm('> ', err)
+                    printm('Backward compatibility : regenerating cache file', fcache)
+                    if exists(fcache):
+                        os.remove(fcache)
+                        
+        # 2nd run to check there are no problems with Energy levels cache files
+        # ... Note @dev : as of 0.9.26 encountering a cache file generated with a future version
+        # ... raises an error with no option to automatically regenerate the cache file
+    
+        try:
+            sf.fetch_databank()
+        except ValueError as err:
+            if "generated with a future version" in str(err) and 'molecules_data' in str(err):
+                # Clean problematic cache files
+                printm('> ', err)
+                printm('Backward compatibility : regenerating energy levels file')
+                sf.params.lvl_use_cached = 'regen'
+                sf.fetch_databank()
+                
+        # Initial calculation to make sure cache files are properly working 
         calc_spectrum(**self.test_options)
 
+    def time_CO2noneq(self):
+        # print('\n'*5)        
+        # print('START')
+
+
+        # from time import time
+        # t0 = time()
+        calc_spectrum(**self.test_options,
+                      export_lines=False,
+                      verbose=3)
+
+        # print('FINISH')
+        # print('{0:.0f}s'.format(time()-t0))
 
     def peakmem_CO2noneq(self):
 
         calc_spectrum(**self.test_options)
 
 
-
-
-c=CO2()
-c.setup()
-c.time_CO2noneq()
