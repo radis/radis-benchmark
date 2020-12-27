05ad709da78dcf1ac74a5fcf1730b7128026b67c
Erwan <erwan.pannier@centralesupelec.fr>
2020-12-28 00:13:56 +0100

    working version with HITEMP tests

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 
 Performance Benchmarks for [RADIS](http://radis.github.io/) 
 
-- [CPU vs GPU calculations](./TEST1.ipynb)
+## Automatic benchmarks 
+
+Benchmarks can be found in [benchmarks/benchmarks.py](./benchmarks/benchmarks.py)
+
+Benchmarks are executed with [Airspeed Velocity](https://asv.readthedocs.io/en/stable/#). 
+Results: 🔗 https://radis.github.io/radis-benchmark/
+
+Run the benchmarks :
+
+```
+asv run HASHFILE:tested_radis_versions.txt -e
+asv publish
+asv preview
+``` 
+
+Benchmarks are executed many times. Some involve calculations of 1+ millions of lines, before the LDM method was introduced, and therefore take a long time. If developing new benchmarks, first check the ASV documentation and in particular ``asv dev`` 
+
+Benchmarks are run against major tagged versions of RADIS. The list of version can be found in [tested_radis_versions.txt](./tested_radis_versions.txt). These tags mostly belong to the [master branch](https://github.com/radis/radis/commits/master). Older versions (< 0.9.21) required manual patches to be able to run the benchmarks. Therefore, support branches were added. See [support/0.9.18](https://github.com/radis/radis/commits/support/0.9.18), [support/0.9.19](https://github.com/radis/radis/commits/support/0.9.19), [support/0.9.21](https://github.com/radis/radis/commits/support/0.9.21), [support/0.9.22](https://github.com/radis/radis/commits/support/0.9.22). 
+
+
+## Manual performance tests :
+
+- [CPU vs GPU calculations](./TEST1.ipynb)  by @pkj-m
--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,116 +1,312 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 
-from radis import calc_spectrum, SpectrumFactory
-from radis import get_version
+import os
+from os.path import join
+from radis import SpectrumFactory, calc_spectrum, get_version
 from radis.misc.printer import printm
 
-class CO2:
+
+class CO2_HITRAN:
     """
-    bla
-    
+    Small performance test based on a reduced dataset of CO2 HITRAN
+
     Also used in :py:func:`radis.test.lbl.test_calc.test_calc_spectrum`
     """
-    
+
     def setup(self):
         self.test_options = opt = {
-            "wavelength_min":4165,
-            "wavelength_max":4200,
-            "databank":"fetch",  # not appropriate for these temperatures, but convenient for automatic testing
-            "Tgas":300,
-            "Tvib":1700,
-            "Trot":1550,
-            "path_length":0.1,
-            "mole_fraction":0.5,
-            "molecule":"CO2",
-            "isotope":"1,2",
-            "wstep":0.01,
-            "cutoff":1e-25,
-            "use_cached":True,
-            "medium":"vacuum",
-            "optimization":"simple",
-            "warnings":{
+            "wavelength_min": 4165,
+            "wavelength_max": 4200,
+            "databank": "fetch",  # not appropriate for these temperatures, but convenient for automatic testing
+            "Tgas": 300,
+            "Tvib": 1700,
+            "Trot": 1550,
+            "path_length": 0.1,
+            "mole_fraction": 0.5,
+            "molecule": "CO2",
+            "isotope": "1,2",
+            "wstep": 0.01,
+            "cutoff": 1e-25,
+            "use_cached": True,
+            "medium": "vacuum",
+            "optimization": "simple",
+            "export_lines": False,
+            "warnings": {
                 "MissingSelfBroadeningWarning": "ignore",
                 "NegativeEnergiesWarning": "ignore",
                 "HighTemperatureWarning": "ignore",
-            }}
-        
+            },
+        }
+
         # Backward compatibility
         # ----------------------
-        
+
         # Old version of RADIS do not necessary work with the latest parameters
-        # Fix it : 
+        # Fix it :
         version = get_version(add_git_number=False)
-        if version < '0.9.26':
-            del self.test_options['optimization']
-        
+        if version < "0.9.26":
+            del self.test_options["optimization"]
+
         # Also fix problems with cache files :
-        
+
         # First run to check there are no problems with Line database cache-files
         # ... Note @dev : as of 0.9.26 encountering a cache file generated with a future version
         # ... raises an error with no option to automatically regenerate the cache file
-        sf = SpectrumFactory(**{k:v for (k,v) in opt.items() if k in [
-            'wavelength_min','wavelength_max', 'molecule', 'isotope', 'broadening_max_width',
-            'medium']})
-        
+        sf = SpectrumFactory(
+            **{
+                k: v
+                for (k, v) in opt.items()
+                if k
+                in [
+                    "wavelength_min",
+                    "wavelength_max",
+                    "molecule",
+                    "isotope",
+                    "broadening_max_width",
+                    "medium",
+                ]
+            }
+        )
+
         try:
             sf.fetch_databank()
         except ValueError as err:
-            if "generated with a future version" in str(err) and 'tempfile' in str(err):
+            if "generated with a future version" in str(err) and "tempfile" in str(err):
                 # Clean problematic cache files
                 # Note @dev : in 0.9.25 fetch_astroquery() doesnt use the use_cache
                 # parameter from SpectrumFactory... Too bad, else we could have simply
                 # used the 'regen' value
                 from astroquery.hitran import Hitran
-                import os
-                from os.path import exists, join
                 from radis.io.query import CACHE_FILE_NAME
-                for iso in sf._get_isotope_list(molecule=opt['molecule']):
+
+                for iso in sf._get_isotope_list(molecule=opt["molecule"]):
                     fcache = join(
                         Hitran.cache_location,
-                        CACHE_FILE_NAME.format(**{
-                        'molecule':opt['molecule'],'isotope':iso,
-                        'wmin':sf.params.wavenum_min_calc,
-                        'wmax':sf.params.wavenum_max_calc}),
+                        CACHE_FILE_NAME.format(
+                            **{
+                                "molecule": opt["molecule"],
+                                "isotope": iso,
+                                "wmin": sf.params.wavenum_min_calc,
+                                "wmax": sf.params.wavenum_max_calc,
+                            }
+                        ),
                     )
-                    printm('> ', err)
-                    printm('Backward compatibility : regenerating cache file', fcache)
-                    if exists(fcache):
-                        os.remove(fcache)
-                        
+                    printm("> ", err)
+                    printm("Backward compatibility : regenerating cache file", fcache)
+                    os.remove(fcache)
+
         # 2nd run to check there are no problems with Energy levels cache files
         # ... Note @dev : as of 0.9.26 encountering a cache file generated with a future version
         # ... raises an error with no option to automatically regenerate the cache file
-    
+
         try:
             sf.fetch_databank()
         except ValueError as err:
-            if "generated with a future version" in str(err) and 'molecules_data' in str(err):
+            if "generated with a future version" in str(
+                err
+            ) and "molecules_data" in str(err):
                 # Clean problematic cache files
-                printm('> ', err)
-                printm('Backward compatibility : regenerating energy levels file')
-                sf.params.lvl_use_cached = 'regen'
+                printm("> ", err)
+                printm("Backward compatibility : regenerating energy levels file")
+                sf.params.lvl_use_cached = "regen"
                 sf.fetch_databank()
-                
-        # Initial calculation to make sure cache files are properly working 
+
+        # Initial calculation to make sure cache files are properly working
         calc_spectrum(**self.test_options)
 
-    def time_CO2noneq(self):
-        # print('\n'*5)        
-        # print('START')
+    def time_noneq_spectrum(self):
 
+        calc_spectrum(**self.test_options)
 
-        # from time import time
-        # t0 = time()
-        calc_spectrum(**self.test_options,
-                      export_lines=False,
-                      verbose=3)
-
-        # print('FINISH')
-        # print('{0:.0f}s'.format(time()-t0))
-
-    def peakmem_CO2noneq(self):
+    def peakmem_noneq_spectrum(self):
 
         calc_spectrum(**self.test_options)
 
 
+class CO2_HITEMP:
+    """
+    Performance test with CO2 HITEMP (2000 - 2250 cm-1 :  ~ 1.5 Million lines )
+
+    Based on the test in :py:func:`radis.test.lbl.test_factory.test_spec_generation`
+    """
+
+    def setup(self):
+        self.test_options = opt = {
+            "wavenum_min": 2000,
+            "wavenum_max": 2250,
+            "molecule": "CO2",
+            "isotope": "1,2,3",
+            "verbose": 3,
+            "wstep": 0.01,
+            "cutoff": 0,
+            "path": [
+                r"D:\Dropbox\Data ECP\14_Databases\CDSD-HITEMP\cdsd_hitemp_07",
+                r"D:\Dropbox\Data ECP\14_Databases\CDSD-HITEMP\cdsd_hitemp_08",
+                r"D:\Dropbox\Data ECP\14_Databases\CDSD-HITEMP\cdsd_hitemp_09",
+            ],
+            "use_cached": True,
+            "dbformat": "cdsd-hitemp",
+        }
+
+        # Backward compatibility
+        # ----------------------
+
+        # Old version of RADIS do not necessary work with the latest parameters
+        # Fix it :
+        version = get_version(add_git_number=False)
+        if version < "0.9.21":
+            opt["dbformat"] = "cdsd"
+
+        # Also fix problems with cache files :
+        from radis.db.utils import getFile
+
+        # First run to check there are no problems with Line database cache-files
+        # ... Note @dev : as of 0.9.26 encountering a cache file generated with a future version
+        # ... raises an error with no option to automatically regenerate the cache file
+        sf = SpectrumFactory(
+            **{
+                k: opt[k]
+                for k in [
+                    "wavenum_min",
+                    "wavenum_max",
+                    "molecule",
+                    "isotope",
+                    "wstep",
+                    "cutoff",
+                    "verbose",
+                ]
+            }
+        )
+
+        for attempt in range(15):  # max number of failed cache files
+            try:
+                sf.load_databank(
+                    path=opt["path"],
+                    format=opt["dbformat"],
+                    parfuncfmt="hapi",
+                    levelsfmt="radis",
+                )
+            except ValueError as err:
+                if "generated with a future version" in str(err):
+                    for fcache in [
+                        opt["path"][0] + ".h5",
+                        opt["path"][1] + ".h5",
+                        opt["path"][2] + ".h5",
+                        getFile(join("CO2", "molecules_data_CO2_iso1_X_levels.h5")),
+                        getFile(join("CO2", "molecules_data_CO2_iso2_X_levels.h5")),
+                    ]:
+                        if fcache in str(err):
+                            printm(
+                                "Backward compatibility : regenerating cache file",
+                                fcache,
+                            )
+                            os.remove(fcache)
+                else:
+                    raise
+            else:
+                break
+
+    def time_eq_spectrum(self):
+        # Note @ dev:  can't use calc_spectrum directly because it cannot
+        # read a custom database
+        opt = self.test_options
+        sf = SpectrumFactory(
+            **{
+                k: opt[k]
+                for k in [
+                    "wavenum_min",
+                    "wavenum_max",
+                    "molecule",
+                    "isotope",
+                    "wstep",
+                    "cutoff",
+                    "verbose",
+                ]
+            }
+        )
+        sf.load_databank(
+            path=opt["path"],
+            format=opt["dbformat"],
+            parfuncfmt="hapi",
+            levelsfmt="radis",
+        )
+        assert len(sf.df0) == 1487308  # number of lines
+        sf.eq_spectrum(Tgas=1700)
+
+    def peakmem_eq_spectrum(self):
+        opt = self.test_options
+        sf = SpectrumFactory(
+            **{
+                k: opt[k]
+                for k in [
+                    "wavenum_min",
+                    "wavenum_max",
+                    "molecule",
+                    "isotope",
+                    "wstep",
+                    "cutoff",
+                    "verbose",
+                ]
+            }
+        )
+        sf.load_databank(
+            path=opt["path"],
+            format=opt["dbformat"],
+            parfuncfmt="hapi",
+            levelsfmt="radis",
+        )
+        assert len(sf.df0) == 1487308  # number of lines
+        sf.eq_spectrum(Tgas=1700)
+
+    def time_noneq_spectrum(self):
+        # Note @ dev:  can't use calc_spectrum directly because it cannot
+        # read a custom database
+        opt = self.test_options
+        sf = SpectrumFactory(
+            **{
+                k: opt[k]
+                for k in [
+                    "wavenum_min",
+                    "wavenum_max",
+                    "molecule",
+                    "isotope",
+                    "wstep",
+                    "cutoff",
+                    "verbose",
+                ]
+            }
+        )
+        sf.load_databank(
+            path=opt["path"],
+            format=opt["dbformat"],
+            parfuncfmt="hapi",
+            levelsfmt="radis",
+        )
+        assert len(sf.df0) == 1487308  # number of lines
+        sf.non_eq_spectrum(Ttrans=300, Tvib=1700, Trot=1550)
+
+    def peakmem_noneq_spectrum(self):
+        opt = self.test_options
+        sf = SpectrumFactory(
+            **{
+                k: opt[k]
+                for k in [
+                    "wavenum_min",
+                    "wavenum_max",
+                    "molecule",
+                    "isotope",
+                    "wstep",
+                    "cutoff",
+                    "verbose",
+                ]
+            }
+        )
+        sf.load_databank(
+            path=opt["path"],
+            format=opt["dbformat"],
+            parfuncfmt="hapi",
+            levelsfmt="radis",
+        )
+        assert len(sf.df0) == 1487308  # number of lines
+        sf.non_eq_spectrum(Ttrans=300, Tvib=1700, Trot=1550)
