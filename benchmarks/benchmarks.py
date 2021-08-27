bc6ba273bd24603b6586460fc3e4cc474280c92f
Erwan Pannier <erwan.pannier@gmail.com>
2021-08-27 02:35:27 +0200

    update benchmarks; add backward compatiblity with 0.10.x, update tested versions

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -12,6 +12,8 @@ from radis import SpectrumFactory, calc_spectrum, get_version
 from radis.lbl.factory import _generate_broadening_range
 from radis.misc.printer import printm
 
+from packaging.version import parse
+
 
 def digits(x, n=1):
     """ Round x to n digits """
@@ -29,7 +31,7 @@ class CO2_HITRAN:
         self.test_options = opt = {
             "wavelength_min": 4165,
             "wavelength_max": 4200,
-            "databank": "fetch",  # not appropriate for these temperatures, but convenient for automatic testing
+            "databank": "hitran",  # not appropriate for these temperatures, but convenient for automatic testing
             "Tgas": 300,
             "Tvib": 1700,
             "Trot": 1550,
@@ -55,8 +57,8 @@ class CO2_HITRAN:
 
         # Old version of RADIS do not necessary work with the latest parameters
         # Fix it :
-        version = get_version(add_git_number=False)
-        if version < "0.9.26":
+        version = parse(get_version(add_git_number=False))
+        if version < parse("0.9.26"):
             del self.test_options["optimization"]
 
         # Also fix problems with cache files :
@@ -124,19 +126,44 @@ class CO2_HITEMP:
             "wavenum_max": 2250,
             "molecule": "CO2",
             "isotope": "1,2,3",
-            "verbose": 3,
+            "verbose": 1,
             "wstep": 0.01,
             "cutoff": 0,
             "chunksize": "auto",
-            "broadening_max_width": 10,
-            "path": [
-                r"/home/gaganaryan/cdsd-hitemp/cdsd_hitemp_07",
-                r"/home/gaganaryan/cdsd-hitemp/cdsd_hitemp_08",
-                r"/home/gaganaryan/cdsd-hitemp/cdsd_hitemp_09",
-            ],
-            "use_cached": True,
-            "dbformat": "cdsd-hitemp",
+            "truncation": 10,
+            "neighbour_lines": 10,
+            "databank": "hitemp",
         }
+
+        # Backward compatibility
+        # ----------------------
+
+        # Old version of RADIS do not necessary work with the latest parameters
+        # Fix it :
+        version = parse(get_version(add_git_number=False))
+
+        if version < parse("0.10.1"):
+            # no automatic download of CO2
+            opt.update({
+                "path": [
+                    r"D:\Dropbox\Data ECP\14_Databases\CDSD-HITEMP\cdsd_hitemp_07",
+                    r"D:\Dropbox\Data ECP\14_Databases\CDSD-HITEMP\cdsd_hitemp_08",
+                    r"D:\Dropbox\Data ECP\14_Databases\CDSD-HITEMP\cdsd_hitemp_09",
+                ],
+                "use_cached": True,
+                "dbformat": "cdsd-hitemp",
+            })
+            del opt["databank"]
+            
+        if version < parse("0.10.1"):
+            # "broadening_max_width" changed to 'truncation' & neighbour_lines
+            opt["broadening_max_widith"] = opt["truncation"]
+            del opt["truncation"]
+            del opt["neighbour_lines"]
+            
+        if version < parse("0.9.21"):
+            # format name changed
+            opt["dbformat"] = "cdsd"
 
         # Chunksize : number of lines to proceed at the same time (doestn apply if LDM)
         if opt["chunksize"] == "auto":
@@ -144,21 +171,12 @@ class CO2_HITEMP:
                 virtual_memory().available
                 / sys.getsizeof(
                     _generate_broadening_range(
-                        opt["wstep"], opt["broadening_max_width"]
+                        opt["wstep"], opt["truncation"]
                     )
                 ),
                 n=1,
             )
             printm("chunksize auto : ", opt["chunksize"])
-
-        # Backward compatibility
-        # ----------------------
-
-        # Old version of RADIS do not necessary work with the latest parameters
-        # Fix it :
-        version = get_version(add_git_number=False)
-        if version < "0.9.21":
-            opt["dbformat"] = "cdsd"
 
         # Also fix problems with cache files :
 
@@ -182,12 +200,17 @@ class CO2_HITEMP:
 
         for attempt in range(15):  # max number of failed cache files
             try:
-                sf.load_databank(
-                    path=opt["path"],
-                    format=opt["dbformat"],
-                    parfuncfmt="hapi",
-                    levelsfmt="radis",
-                )
+                if "databank" in opt:
+                    sf.fetch_databank(opt["databank"],  # hitemp
+                            parfuncfmt="hapi",
+                            levelsfmt="radis",) 
+                else:
+                    sf.load_databank(
+                        path=opt["path"],
+                        format=opt["dbformat"],
+                        parfuncfmt="hapi",
+                        levelsfmt="radis",
+                    )
             except ValueError as err:
                 if "generated with a future version" in str(err):
                     # Get failing cache file :
@@ -213,6 +236,7 @@ class CO2_HITEMP:
 
         sf = SpectrumFactory(
             **{
+                # Default parameters added in SpectrumFactory :
                 k: opt[k]
                 for k in [
                     "wavenum_min",
@@ -226,13 +250,20 @@ class CO2_HITEMP:
                 ]
             }
         )
-        sf.load_databank(
-            path=opt["path"],
-            format=opt["dbformat"],
-            parfuncfmt="hapi",
-            levelsfmt="radis",
-        )
-        assert len(sf.df0) == 1487308  # number of lines
+        # Load databank:
+        if "databank" in opt:
+            sf.fetch_databank(opt["databank"],  # auto-downloaded hitemp
+                    parfuncfmt="hapi",
+                    levelsfmt="radis",) 
+            assert len(sf.df0) == 1464035  #small change with <0.10.1, doesnt change the benchmarks significantly
+        else:
+            sf.load_databank(
+                path=opt["path"],
+                format=opt["dbformat"],
+                parfuncfmt="hapi",
+                levelsfmt="radis",
+            )
+            assert len(sf.df0) == 1487262  # number of lines
         sf.eq_spectrum(Tgas=1700)
 
     def peakmem_eq_spectrum(self):
@@ -252,13 +283,20 @@ class CO2_HITEMP:
                 ]
             }
         )
-        sf.load_databank(
-            path=opt["path"],
-            format=opt["dbformat"],
-            parfuncfmt="hapi",
-            levelsfmt="radis",
-        )
-        assert len(sf.df0) == 1487308  # number of lines
+        # Load databank:
+        if "databank" in opt:
+            sf.fetch_databank(opt["databank"],  # auto-downloaded hitemp
+                    parfuncfmt="hapi",
+                    levelsfmt="radis",)
+            assert len(sf.df0) == 1464035  #small change with <0.10.1, doesnt change the benchmarks significantly
+        else:
+            sf.load_databank(
+                path=opt["path"],
+                format=opt["dbformat"],
+                parfuncfmt="hapi",
+                levelsfmt="radis",
+            )
+            assert len(sf.df0) == 1487308  # number of lines
         sf.eq_spectrum(Tgas=1700)
 
     def time_noneq_spectrum(self):
@@ -280,13 +318,23 @@ class CO2_HITEMP:
                 ]
             }
         )
-        sf.load_databank(
-            path=opt["path"],
-            format=opt["dbformat"],
-            parfuncfmt="hapi",
-            levelsfmt="radis",
-        )
-        assert len(sf.df0) == 1487308  # number of lines
+        # Load databank:
+        if "databank" in opt:
+            sf.fetch_databank(opt["databank"],  # auto-downloaded hitemp
+                    parfuncfmt="hapi",
+                    levelsfmt="radis",) 
+            assert len(sf.df0) == 1464035  #small change with <0.10.1, doesnt change the benchmarks significantly
+        else:
+            sf.load_databank(
+                path=opt["path"],
+                format=opt["dbformat"],
+                parfuncfmt="hapi",
+                levelsfmt="radis",
+            )
+            assert len(sf.df0) == 1487262  # number of lines
+        
+        sf.df0.drop(sf.df0.index[sf.df0['v1u']==-1], inplace=True)
+            
         sf.non_eq_spectrum(Ttrans=300, Tvib=1700, Trot=1550)
 
     def peakmem_noneq_spectrum(self):
@@ -306,11 +354,31 @@ class CO2_HITEMP:
                 ]
             }
         )
-        sf.load_databank(
-            path=opt["path"],
-            format=opt["dbformat"],
-            parfuncfmt="hapi",
-            levelsfmt="radis",
-        )
-        assert len(sf.df0) == 1487308  # number of lines
+        # Load databank:
+        if "databank" in opt:
+            sf.fetch_databank(opt["databank"],  # auto-downloaded hitemp
+                    parfuncfmt="hapi",
+                    levelsfmt="radis",) 
+            assert len(sf.df0) == 1464035  #small change with <0.10.1, doesnt change the benchmarks significantly
+        else:
+            sf.load_databank(
+                path=opt["path"],
+                format=opt["dbformat"],
+                parfuncfmt="hapi",
+                levelsfmt="radis",
+            )
+            assert len(sf.df0) == 1487262  # number of lines
+            
+        sf.df0.drop(sf.df0.index[sf.df0['v1u']==-1], inplace=True)
+            
         sf.non_eq_spectrum(Ttrans=300, Tvib=1700, Trot=1550)
+
+
+if __name__ == '__main__':
+
+    C = CO2_HITEMP()
+    C.setup()
+    from time import perf_counter
+    t0 = perf_counter()
+    C.peakmem_eq_spectrum()
+    print(perf_counter()-t0)
